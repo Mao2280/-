4bb3ee40f3d58626bff9fd08d0fb3ff3fd26fc47
Mao2280 <Sonyaboss228@gmail.com>
2025-03-14 17:00:34 +0300

    белеберда

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('index2')
+@app.route('/index2')
 def index2():
-    return ""
+    return render_template('index2.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/register')
+def register():
+    return render_template('reg.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
